8e4540bb6ad4d5034318bcc651d6515ab0867e8c
Himanshu-tech-pro <110043495+Himanshu-tech-pro@users.noreply.github.com>
2025-03-02 18:55:11 +0530

    Time: 0 ms (100%), Space: 13.7 MB (6.86%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+       int n = citations.size();
+       int h = 0;
+       sort(citations.rbegin() ,citations.rend());
+       for(int i=0; i<n; i++){
+         if(citations[i]>=i+1){
+            h =i+1;
+         }
+         else{
+            break;
+         }
+         
+       }
+       return h; 
+    }
+};
