fe4f555530e6d4af1946ca0032e57d2691393dc1
Himanshu <110043495+Himanshu-tech-pro@users.noreply.github.com>
2025-09-28 12:29:38 +0530

    Time: 0 ms (100%), Space: 8.6 MB (50.35%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,26 +1,12 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        if (x == 0 || x == 1) {
-            return x;  
+        if (x < 2) return x;  
+
+        long long r = x;       
+        while (r * r > x) {
+            r = (r + x / r) / 2;
         }
-        
-        long long left = 0, right = x;
-        
-        while (left <= right) {
-            long long mid = left + (right - left) / 2;
-            long long midSquared = mid * mid;
-            
-            if (midSquared == x) {
-                return mid;  
-            } else if (midSquared < x) {
-                left = mid + 1; 
-            } else {
-                right = mid - 1; 
-            }
-        }
-        
-       
-        return right;
+        return (int) r;
     }
 };
